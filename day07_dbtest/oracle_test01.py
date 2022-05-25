76194c59fca8cd4440dea98e3329c70ac9c2374a
onej2 <cse06039@naver.com>
2022-05-25 17:18:56 +0900

    2022/05/25 중간커밋

--- day07_dbtest/oracle_test01.py
+++ day07_dbtest/oracle_test01.py
@@ -5,10 +5,12 @@ conn = cx_Oracle.connect('scott','tiger','localhost:1521/XE')
 cursor = conn.cursor()
 
 cursor.execute("select * from emp where deptno = 10")
-# print(cursor)
+print(cursor)
 
 for item in cursor:
     print(item[1],item[5])
+
+    print(item)
 
 conn.close()
 
